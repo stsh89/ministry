bc24d0dd62b14901deb20ca87ee147f0474f2696
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-02-08 15:30:03 +0200

    Minor code update

--- src/cli/clap_integration.rs
+++ src/cli/clap_integration.rs
@@ -1,6 +1,9 @@
 use super::{error::CliError, Cli};
 use clap::{Arg, ArgMatches, Command};
 
+const PLAYGROUND_COMMAND: &str = "playground";
+const LOG_SERVER_COMMAND: &str = "log_server";
+
 pub fn get_command() -> Result<Cli, CliError> {
     let command = command();
     let command_name = command.get_name().to_string();
@@ -12,8 +15,8 @@ pub fn get_command() -> Result<Cli, CliError> {
     };
 
     match subcommand {
-        "playground" => get_playground_command(arg_matches),
-        "log_server" => Ok(get_log_server_command()),
+        PLAYGROUND_COMMAND => get_playground_command(arg_matches),
+        LOG_SERVER_COMMAND => Ok(get_log_server_command()),
         name => Err(CliError::command_not_found(name)),
     }
 }
@@ -22,13 +25,13 @@ fn get_playground_command(arg_matches: &ArgMatches) -> Result<Cli, CliError> {
     let subcommand = arg_matches.subcommand();
 
     let Some((subcommand, arg_matches)) = subcommand else {
-        return Err(CliError::missing_subcommand("playground"));
+        return Err(CliError::missing_subcommand(PLAYGROUND_COMMAND));
     };
 
     let cli = match subcommand {
         "list" => get_playground_list_command(),
         "run" => get_playground_run_command(arg_matches)?,
-        name => return Err(CliError::subcommand_not_found("playground", name)),
+        name => return Err(CliError::subcommand_not_found(PLAYGROUND_COMMAND, name)),
     };
 
     Ok(cli)
@@ -61,11 +64,11 @@ fn command() -> clap::Command {
 }
 
 pub fn log_server_command() -> Command {
-    Command::new("log_server").about("Start the server to check the incoming request data")
+    Command::new(LOG_SERVER_COMMAND).about("Start the server to check the incoming request data")
 }
 
 pub fn playground_command() -> Command {
-    Command::new("playground")
+    Command::new(PLAYGROUND_COMMAND)
         .about("Platform for running experimental code")
         .subcommand_required(true)
         .subcommand(playground_list_command())
