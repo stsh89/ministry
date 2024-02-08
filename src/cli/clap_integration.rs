8ce1c5406a39eabc7309b8c4cf5949ef80864fdf
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-02-08 17:05:03 +0200

    Mock `github` cli command

--- src/cli/clap_integration.rs
+++ src/cli/clap_integration.rs
@@ -3,6 +3,7 @@ use clap::{Arg, ArgMatches, Command};
 
 const PLAYGROUND_COMMAND: &str = "playground";
 const LOG_SERVER_COMMAND: &str = "log_server";
+const GITHUB_COMMAND: &str = "github";
 
 pub fn get_command() -> Result<Cli, CliError> {
     let command = command();
@@ -17,8 +18,33 @@ pub fn get_command() -> Result<Cli, CliError> {
     match subcommand {
         PLAYGROUND_COMMAND => get_playground_command(arg_matches),
         LOG_SERVER_COMMAND => Ok(get_log_server_command()),
+        GITHUB_COMMAND => get_github_command(arg_matches),
         name => Err(CliError::command_not_found(name)),
     }
+}
+
+fn get_github_command(arg_matches: &ArgMatches) -> Result<Cli, CliError> {
+    let subcommand = arg_matches.subcommand();
+
+    let Some((subcommand, _arg_matches)) = subcommand else {
+        return Err(CliError::missing_subcommand(PLAYGROUND_COMMAND));
+    };
+
+    let cli = match subcommand {
+        "zen" => get_github_zen_command(),
+        "octocat" => get_github_octocat_command(),
+        name => return Err(CliError::subcommand_not_found(PLAYGROUND_COMMAND, name)),
+    };
+
+    Ok(cli)
+}
+
+fn get_github_octocat_command() -> Cli {
+    todo!()
+}
+
+fn get_github_zen_command() -> Cli {
+    todo!()
 }
 
 fn get_playground_command(arg_matches: &ArgMatches) -> Result<Cli, CliError> {
@@ -58,18 +84,35 @@ fn get_playground_run_command(arg_matches: &ArgMatches) -> Result<Cli, CliError>
 
 fn command() -> clap::Command {
     clap::Command::new("ministry")
-        .subcommand(playground_command())
+        .subcommand(github_command())
         .subcommand(log_server_command())
+        .subcommand(playground_command())
         .subcommand_required(true)
 }
 
-pub fn log_server_command() -> Command {
+fn github_command() -> Command {
+    Command::new(GITHUB_COMMAND)
+        .about("Interact with Github API")
+        .subcommand_required(true)
+        .subcommand(github_zen_command())
+        .subcommand(github_octocat_command())
+}
+
+fn github_zen_command() -> Command {
+    Command::new("zen").about("Receive an inspiring message")
+}
+
+fn github_octocat_command() -> Command {
+    Command::new("octocat").about("Receive a text message from Octocat")
+}
+
+fn log_server_command() -> Command {
     Command::new(LOG_SERVER_COMMAND).about("Start the server to check the incoming request data")
 }
 
-pub fn playground_command() -> Command {
+fn playground_command() -> Command {
     Command::new(PLAYGROUND_COMMAND)
-        .about("Platform for running experimental code")
+        .about("Run any experimental code")
         .subcommand_required(true)
         .subcommand(playground_list_command())
         .subcommand(playground_run_command())
